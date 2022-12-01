22899e742c01620f3993aa96a231d266b23cf708
Lucas Cisneros <lucascisneros00@gmail.com>
2022-11-30 21:47:26 -0500

    init

--- test_1/testapp_1/__init__.py
+++ test_1/testapp_1/__init__.py
@@ -1,3 +1,4 @@
+from ast import Constant
 from otree.api import *
 
 
@@ -8,8 +9,11 @@ Your app description
 
 class C(BaseConstants):
     NAME_IN_URL = 'testapp_1'
-    PLAYERS_PER_GROUP = None
+    PLAYERS_PER_GROUP = 2
     NUM_ROUNDS = 1
+
+    ENDOWMENT = cu(10)
+    MULT_FACTOR = 3
 
 
 class Subsession(BaseSubsession):
@@ -17,17 +21,35 @@ class Subsession(BaseSubsession):
 
 
 class Group(BaseGroup):
+    sent_amount = models.CurrencyField(
+        label = "How much do you want to send?",
+        min = cu(0),
+        max = C.ENDOWMENT
+    )
+    sent_back_amount = models.CurrencyField()
+
     pass
 
 
 class Player(BasePlayer):
     pass
 
+def sent_back_amount_choices(group):
+    return currency_range(
+        cu(0),
+        group.sent_amount * C.MULT_FACTOR,
+        cu(1)
+    )
 
 # PAGES
-class MyPage(Page):
+class Sent(Page):
+    form_model  = "Group"
+    form_fields  = ["fields"]
+
     pass
 
+class SentBack(Page):
+    pass
 
 class ResultsWaitPage(WaitPage):
     pass
